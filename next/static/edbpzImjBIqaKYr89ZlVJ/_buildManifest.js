d5e2e64880d24d9f547d3d0324af9dc460be7711
rdlogout <logoutrd@hotmail.com>
2022-04-17 17:46:21 +0530

    peronal up

--- next/static/edbpzImjBIqaKYr89ZlVJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/signin":[a],"/signup":[a],"/users":[a]}}("static\u002Fchunks\u002Fc1aef7053b35c5ec339c9dab128ec66630c83364.b173f9e628d712f662dc.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
